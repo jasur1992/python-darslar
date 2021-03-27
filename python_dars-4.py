0689fd6dd06349b854cfdc405dff21a1db89ee9c
jasur1992 <81467379+jasur1992@users.noreply.github.com>
2021-03-27 18:15:58 +0500

    Add files via upload

---
+++ python_dars-4.py
@@ -0,0 +1,25 @@
+# 04 dars. O'zgaruvchilar
+# sana 09-03-2021
+# muallif: Bek
+
+
+#ism = "Jasurbek"
+#print(ism)
+
+#ism = "BEkzod"
+#print(ism)
+
+#sm_sharf 'Ergashev Jasurbek'
+
+#vazifa
+
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius ** 2
+# print("Radiusi", radius, "ga teng aylananing Yuzi=", aylana_yuzi)
+
+
+ism = 'Jasur'
+familiya = 'Ergashev'
+ism_sharf = f"{familiya} {ism}"
+print(ism_sharf.title())
---
+++ python_dars-5.py
@@ -0,0 +1,74 @@
+# 06-dars. STRING (MATN)
+# sana: 10/03/2021 yil
+# Muallif: Jasur
+
+# ism = 'Jasur'
+# shahar = 'Qarshi'
+# smayl = '😆'
+# print(smayl)
+# print("Mening ismim " + ism)
+
+# ism = ('Ahad')
+# familiya = ('Qayum')
+# print(ism + familiya)
+# print(ism +' ' + familiya)
+# ism = "Jams"
+# familiya = 'Bond'
+# print(f"Salom, mening ismim {familiya}. {ism} {familiyaisi}!")
+
+# ism = 'jams'
+# familiya = 'bond'
+# ism_sharif = f"{ism} {familiya}"
+# #print(ism_sharif.upper())
+# ism_sharif = ism_sharif.upper()
+# # print(ism_sharif.lower())
+# print(ism_sharif.title())
+# #print(ism_sharif.capitalize())
+               
+# meva = "       olma       "
+# print(meva)
+# print("Men " + meva.lstrip() + "yaxshi ko'raman")
+# print("Men " + meva.rstrip() + " yaxshi ko'raman")
+# print("Men " + meva.strip() + " yaxshi ko'raman")
+# print("Men" + meva + " yaxshi ko'raman")
+
+# ism = input("Ismingizni nima?\n>>>")
+# print("Assalom alaykum, " + ism.title())
+
+
+
+
+
+
+
+###uyga vazifa ###
+
+# kocha = "Bogbon"
+# mahalla = "Sogbon"
+# tuman = "Bodomzor"
+# viloyat = "Samarqand"
+# print(f"{kocha} kochasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+# print("Iltimos malumotingizni kiriting: ")
+# kocha = input("kochangiz: ")
+# maxalla = input("maxallangiz: ")
+# tuman = input("tumaningiz: ")
+# viloyat = input("viloyatingiz: ")
+# #print(kocha + " kochasi,\n " + maxalla + " maxallasi, " + \
+#  #     tuman + " tumani,\n" + viloyat + " viloyati.\n")
+
+
+# manzil = f"{kocha} ko'chasi,\n{maxalla} mahallasi,\n{tuman} tumani,\n{viloyat} viloyati "
+# print(manzil.upper())
+# print(manzil.lower())
+# print(manzil.title())
+# print(manzil.capitalize())
+
+
+
+
+
+
+
+
+                                                                                                                                                     
